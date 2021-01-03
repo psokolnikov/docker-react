1c2915304047b4403b186127ee71af3c581bf361
psokolnikov <sokolnikov@mindbox.ru>
2021-01-03 21:47:06 +0300

    added expose 80

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there 777!
+          Ярик, с Новым годом!
         </p>
         <a
           className="App-link"
